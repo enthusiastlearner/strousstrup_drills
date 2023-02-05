6587d84b7fbf652f1362eb648f2bcda937acc4a3
enthusiastlearner <58307371+enthusiastlearner@users.noreply.github.com>
2023-02-05 22:08:42 +0100

    Update detect_location_of_repeated_adjacent_word.cpp

--- chapter3/detect_location_of_repeated_adjacent_word.cpp
+++ chapter3/detect_location_of_repeated_adjacent_word.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 int main()
 {
-    int number_of_words = 0;
+	int number_of_words = 0;
 	string previous = "";
 	string current;
 	while ( cin >> current)
 	{
-	    number_of_words ++;
-	    if (current == previous)
-	        cout << " word number " << number_of_words << " repeated: " << current << endl;
-	    previous = current;
+		number_of_words ++;
+		if (current == previous)
+			cout << " word number " << number_of_words << " repeated: " << current << endl;
+		previous = current;
 	}
 }
