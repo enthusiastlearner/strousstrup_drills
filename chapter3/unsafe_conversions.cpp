694cf9754b0e7e2bce47f6754cd8b5c71b5b4d54
enthusiastlearner <58307371+enthusiastlearner@users.noreply.github.com>
2023-02-06 19:37:34 +0100

    Update unsafe_conversions.cpp

--- chapter3/unsafe_conversions.cpp
+++ chapter3/unsafe_conversions.cpp
@@ -12,3 +12,8 @@ int main ()
   cout << "c:" << c << endl;
   return 0;
 }
+
+
+// On a PC the range of char values is [–128:127], but only [0:127] can be 
+// used portably because not every computer is a PC, and different computers have different ranges for
+// their char values, such as [0:255].
