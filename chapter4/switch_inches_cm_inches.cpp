59e6430679610fee62887b52950fcd1356d679b9
enthusiastlearner <58307371+enthusiastlearner@users.noreply.github.com>
2023-02-12 19:57:21 +0100

    Create switch_inches_cm_inches.cpp

---
+++ chapter4/switch_inches_cm_inches.cpp
@@ -0,0 +1,33 @@
+// convert from inches to centimeters or centimeters to inches
+// a suffix ‘i’ or ‘c’ indicates the unit of the input
+// any other suffix is an error
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    constexpr double cm_per_inch = 2.54; // cms in an inches
+    
+    double length   = 1;
+    char unit       = '\0';
+    
+    cout << " Please enter a length followed by unit (c for cm and i for inch):\n";
+    cin >> length >> unit;
+    
+    switch(unit)
+    {
+    case 'c':
+        cout << length << "cm = " << length / cm_per_inch << " in\n";
+        break;
+    case 'i':
+        cout << length << "in = " << cm_per_inch * length << " cm\n";
+        break;
+    default:
+        cout << "I do not the specified units\n";
+        break;
+    }
+        
+    return 0;
+}
